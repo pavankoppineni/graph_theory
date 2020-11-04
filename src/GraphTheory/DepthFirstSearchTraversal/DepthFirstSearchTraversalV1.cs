15e3b0e2f07ec9029764ee851dba63f6a529d5de
Koppineni, Pavan K. (Tax&Accounting) <pavan.koppineni@thomsonreuters.com>
2020-11-04 09:37:32 +0530

    Added depth first search

---
+++ src/GraphTheory/DepthFirstSearchTraversal/DepthFirstSearchTraversalV1.cs
@@ -0,0 +1,47 @@
+﻿using GraphTheory.BreadthFirstSearchTraversal;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraphTheory.DepthFirstSearchTraversal
+{
+    public class DepthFirstSearchTraversalV1
+    {
+        private bool[] visited;
+        private IList<int> nodes;
+        private Graph graph;
+
+        public IList<int> Traverse(Graph graph)
+        {
+            this.nodes = new List<int>();
+            this.graph = graph;
+            this.visited = new bool[this.graph.Length];
+            Traverse(0);
+            return nodes;
+        }
+
+        private void Traverse(int nodeIndex)
+        {
+            // Case 1 : Node is visited before
+            // Action : Do nothing
+            if (visited[nodeIndex] == true)
+            {
+                return;
+            }
+            // Case 2 : Node is not visited
+            // Action : Set to true in visited array
+            else
+            {
+                nodes.Add(nodeIndex);
+                visited[nodeIndex] = true;
+                var edgeList = graph[nodeIndex];
+                var currentEdge = edgeList.First;
+                while (currentEdge != null)
+                {
+                    Traverse(currentEdge.Value);
+                    currentEdge = currentEdge.Next;
+                }
+            }
+        }
+    }
+}
---
+++ src/GraphTheory/DepthFirstSearchTraversal/Graph.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraphTheory.BreadthFirstSearchTraversal
+{
+    public class Graph : IEnumerable<LinkedList<int>>
+    {
+        private readonly IList<LinkedList<int>> adjacencyList;
+
+        public Graph(int numberOfNodes)
+        {
+            adjacencyList = new LinkedList<int>[numberOfNodes];
+            for (var i = 0; i < numberOfNodes; i++)
+            {
+                adjacencyList[i] = new LinkedList<int>();
+            }
+        }
+
+        public LinkedList<int> this[int index]
+        {
+            get
+            {
+                return adjacencyList[index];
+            }
+        }
+
+        public void AddEdge(int u, int v)
+        {
+            adjacencyList[u].AddLast(v);
+        }
+
+        public IEnumerator<LinkedList<int>> GetEnumerator()
+        {
+            return adjacencyList.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return adjacencyList.GetEnumerator();
+        }
+
+        public int Length
+        {
+            get
+            {
+                return adjacencyList.Count;
+            }
+        }
+    }
+}
