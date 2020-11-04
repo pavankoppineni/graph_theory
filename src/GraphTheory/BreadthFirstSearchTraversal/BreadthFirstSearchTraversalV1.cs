991e08c25e83d0bd810a4ff4c0c3faa3df3a71e0
Koppineni, Pavan K. (Tax&Accounting) <pavan.koppineni@thomsonreuters.com>
2020-11-04 10:28:17 +0530

    Added breadth first search traversal

---
+++ src/GraphTheory/BreadthFirstSearchTraversal/BreadthFirstSearchTraversalV1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraphTheory.BreadthFirstSearchTraversal
+{
+    /// <summary>
+    /// https://www.youtube.com/watch?v=09_LlHjoEiY&t=1998s
+    /// </summary>
+    public class BreadthFirstSearchTraversalV1
+    {
+        private IList<int> nodes;
+        private bool[] visited;
+        private Graph graph;
+
+        public IList<int> Traverse(Graph graph)
+        {
+            this.graph = graph;
+            this.visited = new bool[graph.Length];
+            this.nodes = new List<int>();
+
+            var queue = new Queue<int>();
+            queue.Enqueue(0);
+            visited[0] = true;
+            this.nodes.Add(0);
+            while (queue.Count > 0)
+            {
+                var currentNode = queue.Dequeue();
+                var edges = graph[currentNode];
+                var currentEdge = edges.First;
+                while (currentEdge != null)
+                {
+                    var node = currentEdge.Value;
+                    // Case 1 : Node is visited
+                    // Action : Do nothing
+                    if (visited[node])
+                    {
+                        currentEdge = currentEdge.Next;
+                        continue;
+                    }
+                    // Case 2 : Node is not visited
+                    // Action : Mark node as visited and enqueue it
+                    else
+                    {
+                        currentEdge = currentEdge.Next;
+                        visited[node] = true;
+                        nodes.Add(node);
+                        queue.Enqueue(node);
+                    }
+                }
+            }
+
+            return nodes;
+        }
+    }
+}
---
+++ test/GraphTheory.Tests/BreadthFirstSearchTraversal/BreadthFirstSearchTraversalV1Tests.cs
@@ -0,0 +1,57 @@
+﻿using GraphTheory.BreadthFirstSearchTraversal;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraphTheory.Tests.BreadthFirstSearchTraversal
+{
+    [TestClass]
+    public class BreadthFirstSearchTraversalV1Tests
+    {
+        [TestMethod]
+        public void Given_Graph_When_BreadthFirstSearchTraversal_Then_ShouldTraverseAllNodesInGraph()
+        {
+            //Given
+            var graph = new Graph(6);
+            graph.AddEdge(0, 1);
+            graph.AddEdge(0, 2);
+
+            graph.AddEdge(1, 0);
+            graph.AddEdge(1, 2);
+            graph.AddEdge(1, 3);
+
+            graph.AddEdge(2, 0);
+            graph.AddEdge(2, 1);
+            graph.AddEdge(2, 5);
+
+            graph.AddEdge(3, 1);
+            graph.AddEdge(3, 4);
+
+            graph.AddEdge(4, 3);
+
+            graph.AddEdge(5, 2);
+
+            var breadthFirstSearch = new BreadthFirstSearchTraversalV1();
+            var expectedResult = new List<int>(6)
+            {
+                0,
+                1,
+                2,
+                3,
+                5,
+                4
+            };
+
+            //When
+            var actualResult = breadthFirstSearch.Traverse(graph);
+
+            //Then
+            Assert.AreEqual(expectedResult.Count, actualResult.Count);
+            for (var i = 0; i < expectedResult.Count; i++)
+            {
+                Assert.AreEqual(expectedResult[i], actualResult[i]);
+            }
+        }
+    }
+}
